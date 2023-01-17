b4c945c9ead46a5b077b539220cc547334b62d02
SalmanAgha <github@tajgasoline.com>
2023-01-17 15:44:48 +0500

    Upper Case CR

--- leftbar.php
+++ leftbar.php
@@ -26,6 +26,18 @@ if (!isset($_SESSION['role'])) {
         '; 
     }
 
+        if($role == 'SuperAdmin'  ){
+        echo ' <li class="nav-item">
+                                    <a class="nav-link"  style="padding:0.8rem 0.1rem !important;" href="#"><i class="ti-bar-chart"></i><span>Customers Management</span><span class="menu-arrow"><i class="mdi mdi-chevron-right"></i></span></a>
+                                
+                                    <ul class="nav-second-level mm-collapse" aria-expanded="false">
+                                        <li><a href="AddCustomer.php">Add Customer</a></li>
+                                        <li><a href="UpdateCustomer.php">Update Customer</a></li>            
+                                    </ul>                             
+                                </li> '; 
+    }
+
+
        if($role == 'SuperAdmin'  ){
         echo '<li id="FBRtransactions"><a style="padding:0.8rem 0rem !important;" href="Home.php"><i class="ti-bar-chart"></i><span> Home</span></i></span></a></li>
 
@@ -51,8 +63,7 @@ if (!isset($_SESSION['role'])) {
  
     ?>
    
-
-         <!-- <li id="FBRtransactions2" ><a style="padding:0.8rem 0rem !important;" href="Customers.php"><i class="ti-bar-chart"></i><span>Customers Management</span></i></span></a></li> -->
+ 
   
   </ul>
 </div>
